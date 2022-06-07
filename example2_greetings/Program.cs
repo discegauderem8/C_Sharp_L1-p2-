1353601ca079fbe38df62ff5a58a15e79785a121
discegauderem8 <discegaudere113399@gmail.com>
2022-06-07 20:33:07 +0300

    программа, приветствующая Машу (с любым регистром)

--- example2_greetings/Program.cs
+++ example2_greetings/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите имя пользователя");
+
+string username = Console.ReadLine();
+
+if (username == "Маша")
+{
+    Console.WriteLine("Ура! Это же МАША!");
+}
+else
+{
+    Console.Write("Привет,");
+    Console.Write(username);
+}
+
+
